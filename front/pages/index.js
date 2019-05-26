1c9ebc44a1bdd97163e2fec1edd7b731a14ee917
RyanCho <mith1004@gmail.com>
2019-05-26 17:01:47 +0900

    antd 추가

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Link from 'next/link';
+import Head from 'next/head';
+import AppLayout from '../components/AppLayout';
 
 const Home = () => {
   return (
     <>
+      <Head>
+        <title>NodeBird</title>
+        <link rel={"stylesheet"} href={"https://cdnjs.cloudflare.com/ajax/libs/antd/3.18.2/antd.min.css"}/>
+        <script src={"https://cdnjs.cloudflare.com/ajax/libs/antd/3.18.2/antd.min.js"}/>
+      </Head>
+    <AppLayout>
       <Link href={"/about"}><a>about</a></Link>
       <div>Hello, Next!</div>
+    </AppLayout>
     </>
   )
 };
