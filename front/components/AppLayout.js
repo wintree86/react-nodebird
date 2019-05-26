7324af31eef4811b088bbffd40d8372b245016c7
RyanCho <mith1004@gmail.com>
2019-05-26 17:40:43 +0900

    기본 페이지 구성

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import {Menu, Input} from "antd";
+import {Menu, Input, Button} from "antd";
+import Link from 'next/link';
 
 const AppLayout = ({children}) => {
   return (
     <div>
       <Menu mode={"horizontal"}>
-        <Menu.Item key={"home"}>NodeBird</Menu.Item>
-        <Menu.Item key={"profile"}>Profile</Menu.Item>
+        <Menu.Item key={"home"}><Link href={"/"}>NodeBird</Link></Menu.Item>
+        <Menu.Item key={"profile"}><Link href={"/profile"}>Profile</Link></Menu.Item>
         <Menu.Item key={"mail"}>
           <Input.Search enterButton style={{verticalAlign:'middle'}}/>
         </Menu.Item>
       </Menu>
+      <Link href="/signup"><Button>Signup</Button></Link>
       {children}
     </div>
   )
--- front/pages/index.js
+++ front/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
 import Head from 'next/head';
 import AppLayout from '../components/AppLayout';
 
@@ -12,7 +11,6 @@ const Home = () => {
         <script src={"https://cdnjs.cloudflare.com/ajax/libs/antd/3.18.2/antd.min.js"}/>
       </Head>
     <AppLayout>
-      <Link href={"/about"}><a>about</a></Link>
       <div>Hello, Next!</div>
     </AppLayout>
     </>
