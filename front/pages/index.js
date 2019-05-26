03a0d677d357d0ae480a72a44ec8544baa5be592
RyanCho <mith1004@gmail.com>
2019-05-26 16:37:04 +0900

    next router 설정

---
+++ front/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Home = () => {
+  return (
+    <>
+      <Link href={"/about"}><a>about</a></Link>
+      <div>Hello, Next!</div>
+    </>
+  )
+};
+
+export default Home;
