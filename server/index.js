3940f58e07db5c972435033cad8a8db3214e01c7
RyanCho <mith1004@gmail.com>
2019-05-29 00:46:21 +0900

    api server setting

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello server');
+})
+
+
+app.listen(8080, () => {
+  console.log('server is running on localhost:8080');
+})
